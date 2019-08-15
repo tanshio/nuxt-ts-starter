2577cb9534b25b0590b75df9a1e341442ac21ec2
tanshio <rakuburomail+tanshio@gmail.com>
2019-08-15 22:37:29 +0900

    storybook

--- src/components/atoms/button/index.stories.ts
+++ src/components/atoms/button/index.stories.ts
@@ -1,7 +1,13 @@
 import { storiesOf } from '@storybook/vue'
 import { withInfo } from 'storybook-addon-vue-info'
 import { action } from '@storybook/addon-actions'
-import { withKnobs, text } from '@storybook/addon-knobs'
+import {
+  withKnobs,
+  text,
+  select,
+  array,
+  optionsKnob as options,
+} from '@storybook/addon-knobs'
 import Button from './index.vue'
 
 storiesOf('Button', module)
@@ -10,12 +16,62 @@ storiesOf('Button', module)
   .add(
     'default',
     () => {
+      const label = 'Colors'
+      const options = {
+        Red: 'red',
+        Blue: 'blue',
+        Yellow: 'yellow',
+        Rainbow: ['red', 'orange', 'etc'],
+        None: null,
+      }
+      const defaultValue = 'red'
+      const groupId = 'GROUP-ID1'
+
+      const value = select(label, options, defaultValue, groupId)
       return {
         components: { Button },
-        template: `<div><Button :text="btnText" @click="onClick" />aaa</div>`,
+        template: `<div><Button :text="btnText" @click="onClick" /></div>`,
         props: {
           btnText: {
-            default: text('text', 'default'),
+            default: value,
+          },
+        },
+        methods: {
+          onClick: action('clicked'),
+        },
+      }
+    },
+    {
+      info: {
+        summary: 'Summary for MyComponent',
+      },
+    },
+  )
+  .add(
+    'size',
+    () => {
+      const label = 'Button size'
+      const valuesObj = {
+        sm: 'sm',
+        primary: 'primary',
+        large: 'large',
+      }
+      const defaultValue = 'primary'
+      const optionsObj = {
+        display: 'select',
+      }
+      const groupId = 'GROUP-ID1'
+
+      const value = options(label, valuesObj, defaultValue, optionsObj, groupId)
+      return {
+        components: { Button },
+        template: `<div><Button :text="btnText" :size="size" @click="onClick" /></div>`,
+        props: {
+          btnText: {
+            default: value,
+          },
+          size: {
+            default: value,
           },
         },
         methods: {
--- src/components/atoms/button/index.vue
+++ src/components/atoms/button/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div @click="handleClick">
-    <p>{{ text }}</p>
-  </div>
+  <button :class="size" @click="handleClick">{{ text }}</button>
 </template>
 
 <script>
 export default {
   props: {
+    size: {
+      type: String,
+      default: 'primary',
+    },
     text: {
       type: String,
       default: '',
@@ -25,7 +27,25 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-div {
-  background: red;
+button {
+  appearance: none;
+  background: #cc0000;
+  color: #fff;
+  text-align: center;
+  display: inline-block;
+  padding: 1rem 1.5rem;
+  font-size: 1rem;
+  border: 0;
+  font-weight: bold;
+  cursor: pointer;
+  p {
+    margin: 0;
+  }
+  &.sm {
+    padding: 0.5rem 0.75rem;
+  }
+  &.large {
+    padding: 2rem 3rem;
+  }
 }
 </style>
